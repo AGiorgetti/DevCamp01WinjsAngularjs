1a428fc80110a5735813de9011e2f9a0179b108d
Alessandro Giorgetti <guardian@nablasoft.com>
2014-05-23 20:22:36 +0200

    all the slides are done!

---
+++ presentation/winjsmetacode.js
@@ -0,0 +1,38 @@
+﻿var product = WinJS.Binding.define({
+    ProductName: "",
+    IsSelected: false
+});
+
+ui.Pages.define("/pages/ListViewBinding/listViewBinding.html", {
+
+    listData: null,
+
+    ready: function (element, options) {
+        this.listData = new WinJS.Binding.List([]); // list wrapper
+        this.createDummyData();
+        var listview = element.querySelector(".mainList").winControl;
+        
+        // setup the listview
+        listview.itemDataSource = this.listData.dataSource;
+        listview.itemTemplate = element.querySelector(".itemtemplate");
+        listview.oniteminvoked = this.itemInvoked.bind(this);
+        listView.layout = new ui.GridLayout();
+    },
+
+    itemInvoked: function (args) {
+        var dataItem = this.listData.getAt(args.detail.itemIndex);
+        dataItem.IsSelected = !dataItem.IsSelected;
+    },
+
+    createDummyData: function () {
+        // Creating dummy data .
+        //Data format : Name, Quantity, SalesPrice, IsSelected 
+
+        this.listData.push(WinJS.Binding.as(new product({ ProductName: "Product1", IsSelected: false })));
+        this.listData.push(WinJS.Binding.as(new product({ ProductName: "Product1", IsSelected: false })));
+        this.listData.push(WinJS.Binding.as(new product({ ProductName: "Product1", IsSelected: false })));
+    }
+});
+
+// somewhere in code ...
+WinJS.Binding.processAll(htmlElement, viewModel);
